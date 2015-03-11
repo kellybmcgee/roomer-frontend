dca97299bee776483050475e833859632a5e407c
kmcgee@mit.edu <kelly@fanpics.com>
2015-03-10 20:21:04 -0400

    Initial Commit

---
+++ Roomer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Roomer
+//
+//  Created by Kelly McGee on 3/10/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Roomer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Roomer
+//
+//  Created by Kelly McGee on 3/10/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
